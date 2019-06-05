3d74c379e56496cc16cc0e66ae5c770595ebabfe
Alex Merecka <merecka@gmail.com>
2019-06-05 18:49:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new
+    # time = Time.new
+    # 
+    # resp.write "#{time}\n"
 
-    resp.write "#{time}\n"
-
-    if time.hour < 12
+    if Time.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
